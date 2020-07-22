b23a1454a9cc3b98534750faca571e40cab56503
John Onyango <j.yayah7@gmail.com>
2020-07-22 12:56:10 +0300

    one test successful

--- src/com/company/StudentTest.java
+++ src/com/company/StudentTest.java
@@ -1,7 +1,24 @@
 package com.company;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
+    Student s=new Student();
+    @Test
+    public void setMarks() {
 
+        int marks=70;
+        assertEquals(70, marks);
+
+    }
+    @Test
+    public void setGrade() {
+
+        char b='a';
+        assertEquals('a', b);
+
+
+    }
 }
